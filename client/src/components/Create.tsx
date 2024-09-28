e127cf02dc674c2079350d491dc8b60adab6e9eb
tonydnh <tonydinh442@gmail.com>
2024-09-28 01:05:39 -0500

    Update category creation card

--- client/src/components/Create.tsx
+++ client/src/components/Create.tsx
@@ -50,56 +50,58 @@ export default function Create() {
   }
 
   return (
-    <div className="p-6">
-      <div className="font-bold text-3xl md:text-5xl">  
-        Custom Spending Category
+    <div className="h-full overflow-y-hidden flex justify-center">
+      <div className="h-fit w-[32rem] mt-24 p-6 shadow-md border border-slate-300 rounded-md">
+        <div className="font-bold text-2xl md:text-4xl">  
+          Create Spending Category
+        </div>
+        <div className="text-xl mb-4">
+          Track your spending on custom categories.
+        </div>
+
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+          <div className="w-full flex flex-col gap-1"> 
+            <label htmlFor="categoryName">Category Name</label>
+            <input 
+              type="text"
+              {...register("categoryName", { required: "This is required." })}
+              id="categoryName"
+              className="border border-slate-300 rounded py-1.5 pl-1 text-slate-900 placeholder:text-slate-400"
+              placeholder="Ex: Groceries"
+            />
+            <p className="text-red-500">{typeof errors.categoryName?.message === "string" ? errors.categoryName.message : ""}</p>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="description">Description</label>
+            <textarea
+              {...register("description", { required: "This is required." })}
+              id="description"
+              className="border border-slate-300 rounded py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 resize-none"
+              placeholder="Describe the category"
+            />
+            <p className="text-red-500">{typeof errors.description?.message === "string" ? errors.description.message : ""}</p>
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label htmlFor="color">Color</label>
+            <input
+              type="color"
+              {...register("color")}
+              id="color"
+              defaultValue="#2a9df4"
+              className="rounded-sm"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-36 self-center px-8 py-3 mt-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 disabled:bg-blue-300 shadow-sm"
+          >
+            Create
+          </button>
+        </form>
       </div>
-      <div className="text-xl mb-4">
-        Track your spending on custom categories.
-      </div>
-
-      <form className="flex flex-col gap-3 max-w-sm" onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col gap-1"> 
-          <label htmlFor="categoryName">Category Name</label>
-          <input 
-            type="text"
-            {...register("categoryName", { required: "This is required." })}
-            id="categoryName"
-            className="border border-slate-300 rounded py-1.5 pl-1 text-slate-900 placeholder:text-slate-400"
-            placeholder="Ex: Apartment Spending"
-          />
-          <p className="text-red-500">{typeof errors.categoryName?.message === "string" ? errors.categoryName.message : ""}</p>
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <label htmlFor="description">Description</label>
-          <textarea
-            {...register("description", { required: "This is required." })}
-            id="description"
-            className="border border-slate-300 rounded py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 resize-none"
-            placeholder="Describe the category"
-          />
-          <p className="text-red-500">{typeof errors.description?.message === "string" ? errors.description.message : ""}</p>
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <label htmlFor="color">Color</label>
-          <input
-            type="color"
-            {...register("color")}
-            id="color"
-            defaultValue="#2a9df4"
-            className="rounded-sm"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="w-36 px-8 py-3 mt-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 disabled:bg-blue-300 shadow-sm"
-        >
-          Create
-        </button>
-      </form>
     </div>
   );
 }
